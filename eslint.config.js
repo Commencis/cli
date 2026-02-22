1720706977eb1365cd2f3c87c574ca93436d4639
Mehmetcan Yılmaz <github@mcan.me>
2026-02-22 20:11:58 +0300

    chore: update dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,8 @@
 import { typescriptConfig } from '@commencis/eslint-config';
 import { defineConfig } from 'eslint/config';
 
-export default defineConfig(typescriptConfig);
+export default defineConfig(typescriptConfig, {
+  rules: {
+    'no-console': 'off',
+  },
+});
